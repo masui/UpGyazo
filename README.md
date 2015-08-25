2604f9fab13eff0626806024a1d8388775470310
Toshiyuki Masui <masui@pitecan.com>
2015-08-25 20:27:47 +0100

    ...

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # ファイルをアップロードしてGyazoに登録
+
+Usage: ```% ruby upgyazo ファイル```
+
+* 文書や写真のサムネをGyazoにアップロードして本体は```masui.sfc.keio.ac.jp```に格納する。
+
--- register.rb
+++ register.rb
@@ -1,9 +1,12 @@
+# coding: utf-8
+#
+# Gyazo.cool環境で動かす
+#
 id = ARGV[0]
 time = ARGV[1]
 url = ARGV[2]
 exit unless id && time && url
 
-#time = time.gsub(/_/,' ')
 d = Image.find_by(image_id: id)
 d.date = Time.zone.parse(time).utc
 d.metadata = { 'url' => url }
