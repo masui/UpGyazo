49535b3c4876a90e0bab73f5d71f37a949a23c00
Toshiyuki Masui <masui@pitecan.com>
2015-09-06 05:09:23 +0900

    ...

--- upgyazo.rb
+++ upgyazo.rb
@@ -43,7 +43,11 @@ class GyazoUpload
     # サムネ生成
     #
     if jpeg?(file)
-      system "convert -resize 400x '#{file}' #{thumbfile}"
+      begin
+        system "convert -resize 400x '#{file}' #{thumbfile}"
+      rescue
+        thumbfile = nil
+      end
     else
       system "qlmanage -t -o /tmp -s 200 #{file} > /dev/null 2> /dev/null"
       system "/bin/mv '/tmp/#{File.basename(file)}.png' #{thumbfile} > /dev/null 2> /dev/null"
@@ -77,24 +81,26 @@ class GyazoUpload
   def process(file)
     STDERR.puts "Generating thumbnail..."
     thumbfile = generate_thumbnail(file)    # いろんな方法でサムネイル生成
-    STDERR.puts "Uploading to Gyazo..."
-    gyazoid = upload_and_delete(thumbfile)  # サムネイルをGyazoにアップロードしてサムネイルファイルは削除
-    (dstfile, dsturl) = dst(file)   # オリジナルファイルのコピー先ファイル名, URLを取得
-    #
-    # クラウドにオリジナルファイルをコピー
-    #
-    STDERR.puts "Copying original file <#{file}> to masui.sfc.keio.ac.jp..."
-    # system "scp #{file} masui.sfc.keio.ac.jp:#{dstfile} > /dev/null 2> /dev/null"
-    # system "ssh masui.sfc.keio.ac.jp chmod 644 #{dstfile} > /dev/null 2> /dev/null"
-    #
-    # nota@gyazo.cool のプログラムを使って、日付とURLを登録
-    #
-    STDERR.puts "Setting dates and urls on Gyazo.com..."
-    time = modtime(file) # EXIFの撮影時刻またはファイル修正時刻
-    command = "ssh nota@gyazo.cool sh /home/nota/masui/register '#{gyazoid}' '#{time.to_s.gsub(/ /,"\\ ")}' '#{dsturl}' > /dev/null 2> /dev/null"
-    system command
-
-    puts "#{gyazoid}\t#{time}"
+    if thumbfile then
+      STDERR.puts "Uploading to Gyazo..."
+      gyazoid = upload_and_delete(thumbfile)  # サムネイルをGyazoにアップロードしてサムネイルファイルは削除
+      (dstfile, dsturl) = dst(file)   # オリジナルファイルのコピー先ファイル名, URLを取得
+      #
+      # クラウドにオリジナルファイルをコピー
+      #
+      STDERR.puts "Copying original file <#{file}> to masui.sfc.keio.ac.jp..."
+      system "scp #{file} masui.sfc.keio.ac.jp:#{dstfile} > /dev/null 2> /dev/null"
+      system "ssh masui.sfc.keio.ac.jp chmod 644 #{dstfile} > /dev/null 2> /dev/null"
+      #
+      # nota@gyazo.cool のプログラムを使って、日付とURLを登録
+      #
+      STDERR.puts "Setting dates and urls on Gyazo.com..."
+      time = modtime(file) # EXIFの撮影時刻またはファイル修正時刻
+      command = "ssh nota@gyazo.cool sh /home/nota/masui/register '#{gyazoid}' '#{time.to_s.gsub(/ /,"\\ ")}' '#{dsturl}' > /dev/null 2> /dev/null"
+      system command
+      
+      puts "#{gyazoid}\t#{time}"
+    end
   end
 end
 
